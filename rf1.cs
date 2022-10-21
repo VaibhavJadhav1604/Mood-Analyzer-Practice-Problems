6fdba7e8756df44e084f25a939a2595ecfedcfc4
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-22 00:29:09 +0530

    Test Case File

---
+++ rf1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using REFACTOR1;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class rf1
+    {
+        public RF1 rf;
+        [TestMethod]
+        public void TM1()
+        {
+            string expected = "SAD";
+            string msg = "I Am SAD";
+            rf = new RF1(msg);
+            string value = rf.analysemood();
+            Assert.AreEqual(expected, value);
+        }
+        [TestMethod]
+        public void TM11()
+        {
+            string expected = "HAPPY";
+            string msg = "I Am HAPPY";
+            rf = new RF1(msg);
+            string value = rf.analysemood();
+            Assert.AreEqual(expected, value);
+        }
+    }
+}
