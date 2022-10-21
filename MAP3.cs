7ba92bdc6b0d04ee6e78bb326fc9ab1f00a8c020
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-21 23:53:16 +0530

    Inform User If Mood Is Null Or Empty By Exception
    Inform User If Mood Is Null Or Empty By Exception

---
+++ MAP3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MAPP3
+{
+    public class MoodAnalysisException : Exception
+    {   
+        public enum ExceptionType
+        {
+            Null,Empty
+        }
+
+        public readonly ExceptionType type; 
+        
+        public MoodAnalysisException(ExceptionType type,string msg)
+        : base(msg)
+        {
+            this.type = type;
+        }
+    }
+    public class MAP3
+    {
+        public string analysemood(string msg)
+        {
+            try
+            {
+                if (msg.Equals(string.Empty))
+                {
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.Empty,"Empty Value");
+                }
+                if (msg.Contains("SAD"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch(NullReferenceException)
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.Null,"Null Value");
+            }
+        }
+    }
+    internal class MAPP3
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
