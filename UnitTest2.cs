7e46bf4b35ea8c8e2884d589721fa24b1a331863
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-22 00:28:29 +0530

    Test Case File

---
+++ UnitTest2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MAPP2;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        public MAP2 map2;
+        [TestMethod]
+        [DataRow(null)]
+        public void TestMethod2(string msg)
+        {
+            map2 = new MAP2();
+            string expected = "HAPPY";
+            string value = map2.analysemood(msg);
+            Assert.AreEqual(expected, value);
+        }
+    }
+}
