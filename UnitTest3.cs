27e888e964693a06d6b6e83119af3982b907b933
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-22 00:28:48 +0530

    Test Case File

---
+++ UnitTest3.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MAPP3;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        public MAP3 map3;
+        [TestMethod]
+        public void EmptyMsg()
+        {
+            map3 = new MAP3();
+            try
+            {
+                string msg = "";
+                string value = map3.analysemood(msg);
+            }
+            catch (MoodAnalysisException e)
+            {
+                Assert.AreEqual("Empty Value", e.Message);
+            }
+        }
+        [TestMethod]
+        public void NullMsg()
+        {
+            map3 = new MAP3();
+            try
+            {
+                string msg = null;
+                string value = map3.analysemood(msg);
+            }
+            catch(MoodAnalysisException e)
+            {
+                Assert.AreEqual("Null Value", e.Message);
+            }
+        }
+    }
+}
