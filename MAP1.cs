671cd08c3b435d33c114ab66a4c0cfc1477f8765
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-21 21:04:36 +0530

    Ability To Analyze And Respond Happy Or Sad Mood
    Ability To Analyze And Respond Happy Or Sad Mood

---
+++ MAP1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MAPP1
+{
+    public class MAP1
+    {
+        public string analysemood(string msg)
+        {
+            if (msg.Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+    internal class MAPP1
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
