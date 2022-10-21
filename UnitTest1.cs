aff9471278b834864486afb13bd56637281c83ce
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-22 00:25:20 +0530

    Test Case File

---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MAPP1;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public MAP1 map1;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            map1 = new MAP1();
+            string expected = "SAD";
+            string msg = "I Am SAD";
+            string value = map1.analysemood(msg);
+            Assert.AreEqual(expected, value);
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            map1 = new MAP1();
+            string expected = "HAPPY";
+            string msg = "I Am HAPPY";
+            string value = map1.analysemood(msg);
+            Assert.AreEqual(expected, value);
+        }
+    }
+}
